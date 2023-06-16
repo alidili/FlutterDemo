2636a9b9b4bf4b497a5dd2a5ba3bb108d8a4be1e
yangle <mr.yang0229@foxmail.com>
2023-06-16 15:00:44 +0800

    增加flutter_lib模块

--- app/src/main/java/tech/yangle/flutter/MainActivity.kt
+++ app/src/main/java/tech/yangle/flutter/MainActivity.kt
@@ -3,10 +3,17 @@ package tech.yangle.flutter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Flutter
+ * <p>
+ * Created by YangLe on 2023/6/14.
+ * Website：http://www.yangle.tech
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
