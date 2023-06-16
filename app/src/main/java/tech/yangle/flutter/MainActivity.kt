c7dc511d3b129bcf0c3092eb6e319ec4f7d30c02
yangle <mr.yang0229@foxmail.com>
2023-06-16 16:40:33 +0800

    原生跳转Flutter逻辑

--- app/src/main/java/tech/yangle/flutter/MainActivity.kt
+++ app/src/main/java/tech/yangle/flutter/MainActivity.kt
@@ -1,7 +1,10 @@
 package tech.yangle.flutter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
 
 /**
  * Flutter
@@ -9,11 +12,19 @@ import android.os.Bundle
  * Created by YangLe on 2023/6/14.
  * Website：http://www.yangle.tech
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.btn_flutter).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = FlutterActivity
+            .withCachedEngine("default_engine_id")
+            .build(this)
+        startActivity(intent)
     }
 }
